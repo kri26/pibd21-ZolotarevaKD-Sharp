f9c7cbf72a25a60db10fc901a59cfff1f0f7a8a7
Золотарева Кристина Денисовна <kristina.zolotareva.14@gmail.com>
2019-10-27 17:47:14 +0400

    Лабораторная 3

---
+++ WindowsFormsBoat/FormParking.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsBoat
+{
+    partial class FormParking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxParking = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxTakeBoat = new System.Windows.Forms.PictureBox();
+            this.buttonTakeBoat = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSetSportBoat = new System.Windows.Forms.Button();
+            this.buttonSetBoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBoat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxParking
+            // 
+            this.pictureBoxParking.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxParking.Name = "pictureBoxParking";
+            this.pictureBoxParking.Size = new System.Drawing.Size(995, 493);
+            this.pictureBoxParking.TabIndex = 7;
+            this.pictureBoxParking.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxTakeBoat);
+            this.groupBox1.Controls.Add(this.buttonTakeBoat);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(1027, 269);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 236);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Забрать катер";
+            // 
+            // pictureBoxTakeBoat
+            // 
+            this.pictureBoxTakeBoat.Location = new System.Drawing.Point(9, 125);
+            this.pictureBoxTakeBoat.Name = "pictureBoxTakeBoat";
+            this.pictureBoxTakeBoat.Size = new System.Drawing.Size(139, 96);
+            this.pictureBoxTakeBoat.TabIndex = 3;
+            this.pictureBoxTakeBoat.TabStop = false;
+            // 
+            // buttonTakeBoat
+            // 
+            this.buttonTakeBoat.Location = new System.Drawing.Point(35, 79);
+            this.buttonTakeBoat.Name = "buttonTakeBoat";
+            this.buttonTakeBoat.Size = new System.Drawing.Size(75, 23);
+            this.buttonTakeBoat.TabIndex = 2;
+            this.buttonTakeBoat.Text = "Забрать";
+            this.buttonTakeBoat.UseVisualStyleBackColor = true;
+            this.buttonTakeBoat.Click += new System.EventHandler(this.buttonTakeBoat_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(79, 38);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(57, 22);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Место";
+            // 
+            // buttonSetSportBoat
+            // 
+            this.buttonSetSportBoat.Location = new System.Drawing.Point(1055, 91);
+            this.buttonSetSportBoat.Name = "buttonSetSportBoat";
+            this.buttonSetSportBoat.Size = new System.Drawing.Size(120, 67);
+            this.buttonSetSportBoat.TabIndex = 5;
+            this.buttonSetSportBoat.Text = "Припарковать спортивный катер";
+            this.buttonSetSportBoat.UseVisualStyleBackColor = true;
+            this.buttonSetSportBoat.Click += new System.EventHandler(this.buttonSetSportBoat_Click);
+            // 
+            // buttonSetBoat
+            // 
+            this.buttonSetBoat.Location = new System.Drawing.Point(1055, 31);
+            this.buttonSetBoat.Name = "buttonSetBoat";
+            this.buttonSetBoat.Size = new System.Drawing.Size(120, 54);
+            this.buttonSetBoat.TabIndex = 4;
+            this.buttonSetBoat.Text = "Припарковать катер";
+            this.buttonSetBoat.UseVisualStyleBackColor = true;
+            this.buttonSetBoat.Click += new System.EventHandler(this.buttonSetBoat_Click);
+            // 
+            // FormParking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 517);
+            this.Controls.Add(this.pictureBoxParking);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSetSportBoat);
+            this.Controls.Add(this.buttonSetBoat);
+            this.Name = "FormParking";
+            this.Text = "FormParking";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBoat)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxParking;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxTakeBoat;
+        private System.Windows.Forms.Button buttonTakeBoat;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSetSportBoat;
+        private System.Windows.Forms.Button buttonSetBoat;
+    }
+}
